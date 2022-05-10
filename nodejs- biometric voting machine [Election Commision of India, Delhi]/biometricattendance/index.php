e23323ecc0fdb4f7c675be0cb1e0867cde0b0044
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-05-11 01:23:35 +0530

    Update index.php

--- nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/index.php
+++ nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/index.php
@@ -3,7 +3,7 @@
 <head>
   <title>Users</title>
 <link rel="stylesheet" type="text/css" href="css/Users.css">
-<script>
+<script> 
   $(window).on("load resize ", function() {
     var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width(); 
     $('.tbl-header').css({'padding-right':scrollWidth});

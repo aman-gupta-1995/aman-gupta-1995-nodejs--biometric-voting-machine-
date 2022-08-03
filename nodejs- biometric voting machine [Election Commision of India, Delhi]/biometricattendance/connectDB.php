82f930b18944e6df3cb97c0b498e3bf6e9101df8
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-03 19:01:15 +0530

    Update connectDB.php

--- nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/connectDB.php
+++ nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/connectDB.php
@@ -4,7 +4,7 @@
     $username = "root";		//put your phpmyadmin username.(default is "root")
     $password = "";			//if your phpmyadmin has a password put it here.(default is "root") 
     $dbname = "biometricattendace"; 
-    
+ 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
 	if ($conn->connect_error) {

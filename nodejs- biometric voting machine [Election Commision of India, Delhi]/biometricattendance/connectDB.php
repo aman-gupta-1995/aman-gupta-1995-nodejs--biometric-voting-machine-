d1048c95d0286eeb517d5205c3ef43bb25b8bbcc
AMAN GUPTA <amangupta1995.ag@gmail.com>
2022-08-03 19:01:41 +0530

    Update connectDB.php

--- nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/connectDB.php
+++ nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/connectDB.php
@@ -6,7 +6,7 @@
     $dbname = "biometricattendace"; 
  
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
-	
+
 	if ($conn->connect_error) { 
         die("Database Connection failed: " . $conn->connect_error);   
     

28e9f92c45eaa06e16b56c32613cc626366b1925
AMAN GUPTA <amangupta1995.ag@gmail.com>
2023-02-08 06:35:40 +0530

    Update install.php

--- nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/install.php
+++ nodejs- biometric voting machine [Election Commision of India, Delhi]/biometricattendance/install.php
@@ -3,12 +3,12 @@
     $servername = "localhost";
     $username = "root";		//put your phpmyadmin username.(default is "root")
     $password = "";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "";
+    $dbname = ""; 
     
 	$conn = new mysqli($servername, $username, $password, $dbname);
 
 	// Create database 
-	$sql = "CREATE DATABASE biometricattendace";
+	$sql = "CREATE DATABASE biometricattendace"; 
 	if ($conn->query($sql) === TRUE) {
 	    echo "Database created successfully";
 	} else {
